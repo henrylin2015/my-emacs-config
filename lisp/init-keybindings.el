be420957d05e520b460220ec87400b00f83190c3
henrylin2015 <lin_hailing@sina.com>
2016-08-21 00:14:08 +0800

    add hippie补全功能的使用等

--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -9,5 +9,7 @@
 ;;然后再将其用下面的代码将其绑定为快捷键，第一个 \ 用于将紧跟的 \ 进行逃脱（escape）
 (global-set-key (kbd "C-M-\\") 'indent-region-or-buffer)
 
+;;hippie auto company Hippie 补全
+(global-set-key (kbd "s-/") 'hippie-expand)
 
 (provide 'init-keybindings)
